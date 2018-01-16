bf312164e93b3b1636997bc02fcee28d8277af0a
Bence Darázs <bence.darazs@econ.local>
2018-01-16 12:39:38 +0100

    Initial commit

---
+++ rename-doms.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import re
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter.font import Font
+from tkinter import messagebox
+from tkinter import simpledialog
+import shutil
+from shutil import copyfile
+import math
+import os
+
+class MainApplication(tk.Frame):
+    pass
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    MainApplication(root).pack()
+    root.mainloop()
